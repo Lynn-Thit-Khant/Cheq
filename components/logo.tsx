7bcea4ea188f6b2adc28ba81ad3ec7400f10ac48
Lynn Thit Khant <lynntk.sg@gmail.com>
2026-08-06 14:36:42 +0800

    fix: use dark:invert on Logo component for guaranteed white badge in dark mode

--- components/logo.tsx
+++ components/logo.tsx
@@ -20,23 +20,13 @@ export function Logo({ className, size = "md" }: LogoProps) {
 
   return (
     <div className={cn("inline-flex items-center justify-center shrink-0 select-none", className)}>
-      {/* Light mode: Black logo badge */}
       <Image
         src="/logo-dark.svg"
         alt="Cheq Logo"
         width={px}
         height={px}
         priority
-        className="object-contain shrink-0 dark:hidden"
-      />
-      {/* Dark mode: White logo badge */}
-      <Image
-        src="/logo-light.svg"
-        alt="Cheq Logo"
-        width={px}
-        height={px}
-        priority
-        className="object-contain shrink-0 hidden dark:block"
+        className="object-contain shrink-0 dark:invert"
       />
     </div>
   )
